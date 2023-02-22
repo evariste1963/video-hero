81f55c901ca89a1d97dfed148cbdfa04faa97d72
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2023-02-22 09:54:48 +0000

    fiddling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,16 +23,16 @@
 		position: sticky;
 		top: 0;
 		z-index: 999;
-		margin:0;
+		margin: 0;
 		text-align: center;
-		background: rgba(9, 127, 148, 1);
+		background: rgba(34, 152, 173, 1);
 		box-shadow: 0 0 10px 0 rgb(0 0 0);
 		backface-visibility: hidden;
 	}
 	nav ul {
 		display: flex;
 		justify-content: center;
-		margin:0;
+		margin: 0;
 	}
 
 	nav a {
--- src/routes/+page.js
+++ src/routes/+page.js
@@ -5,12 +5,12 @@ const videos = import.meta.glob('$lib/hero/videos/*/*.mp4');
  */
 let vidArray = [];
 
-export function load() {
+export async function load() {
 	for (const path in videos) {
 		vidArray.push(path);
 	}
 
-	let video = vidArray[Math.floor(Math.random() * vidArray.length)];
+	let video = await vidArray[Math.floor(Math.random() * vidArray.length)];
 
 	return {
 		video
