b36f2f19d7c470e83748d3b22ed5c0bad7c38625
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2023-02-24 14:50:07 +0000

    gdg

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -7,7 +7,6 @@
 		margin: 1em 5em;
 		background: rgba(243, 240, 240, 0.3);
 		padding: 15px 15px 0px 15px;
-
 		border-top-left-radius: 1em;
 		border-top-right-radius: 1em;
 		box-shadow: 2px 4px rgba(0, 0, 0, 0.4);
