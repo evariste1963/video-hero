87aaac73dcd8b5bb27abfe4d77afe6fae142d7cc
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2023-02-24 10:55:18 +0000

    moved spinner code to Loading and changed colour

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -7,51 +7,51 @@
 		{ page: 'Events', path: 'events' },
 		{ page: 'Contact', path: 'contact' }
 	];
-
 </script>
-	<nav>
-		
-			{#each navItems as navItem}
-				{#if navItem.page !== 'Home'}
-					<a
-						href={navItem.path}
-						class:selected={$page.route.id.includes(navItem.path)}
-						data-sveltekit-preload-data>{navItem.page}</a
-					>
-				{:else}
-					<a href={navItem.path} 
-					class:selected = {$page.route.id.split('/')[1] === ("")}
-					data-sveltekit-preload-data
-					>{navItem.page} </a>
-				{/if}
-			{/each}
-		
-	</nav>
-	
+
+<nav>
+	{#each navItems as navItem}
+		{#if navItem.page !== 'Home'}
+			<a
+				href={navItem.path}
+				class:selected={$page.route.id.includes(navItem.path)}
+				data-sveltekit-preload-data>{navItem.page}</a
+			>
+		{:else}
+			<a
+				href={navItem.path}
+				class:selected={$page.route.id.split('/')[1] === ''}
+				data-sveltekit-preload-data
+				>{navItem.page}
+			</a>
+		{/if}
+	{/each}
+</nav>
+
 <style>
 	nav {
 		position: fixed;
 		width: 100vw;
-		display:flex;
+		display: flex;
 		top: 0;
 		z-index: 999;
 		margin: 0;
 		text-align: center;
 		justify-content: center;
-		background: rgba(96, 107, 109, .95);
+		background: rgba(97, 108, 110, 0.99);
 		box-shadow: 0 0 10px 0 rgb(0 0 0);
 		/*backface-visibility: hidden;*/
 	}
-	
+
 	nav a {
 		margin: 0.4% 1em;
 		text-decoration: none;
 		color: rgb(255, 255, 255);
-		font-size:calc(12px + 0.5vw);
+		font-size: calc(12px + 0.5vw);
 		padding: calc(0.1em + 0.3vw) 0;
 	}
 
-	.selected {		
+	.selected {
 		font-weight: bold;
 		border-bottom: 1px solid #fff;
 	}
