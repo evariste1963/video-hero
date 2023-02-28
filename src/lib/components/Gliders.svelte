b980e3e574015570d7b6e04889fdcf00525dc934
Royston Allfrey DNA UK <Royston.Allfrey@dncompany.com>
2023-02-28 16:19:57 +0000

    work again

--- src/lib/components/Gliders.svelte
+++ src/lib/components/Gliders.svelte
@@ -65,7 +65,7 @@
 		padding: 1rem 1rem;
 		border-radius: 0.7rem;
 		margin: auto 1rem;
-		color: #fff;
+		color: rgba(255, 255, 255, 0.7);
 		background-color: rgba(97, 108, 110, 0.7);
 		cursor: pointer;
 		transition: color 1s ease;
