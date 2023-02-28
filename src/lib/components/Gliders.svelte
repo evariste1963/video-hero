b2c8d9496e8ea6c01448856e8f62173674795449
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2023-02-28 11:31:06 +0000

    white gliders

--- src/lib/components/Gliders.svelte
+++ src/lib/components/Gliders.svelte
@@ -9,18 +9,24 @@
 	}
 </script>
 
-<!-- <div class="glider-bar"> -->
-	<gliders>
-		{#each gliders as glider, i}
-			<a href=".section{i + 1}" on:click|preventDefault={scrollIntoView} class="button-89"
-				>{glider}</a
-			>
-		{/each}
-	</gliders>
-<!-- </div> -->
+<div class="glider-bar" />
+<gliders>
+	{#each gliders as glider, i}
+		<a href=".section{i + 1}" on:click|preventDefault={scrollIntoView} class="button-89">{glider}</a
+		>
+	{/each}
+</gliders>
 
+<!-- </div> -->
 <style>
-	
+	.glider-bar {
+		position: absolute;
+		top: 60%;
+		width: 100%;
+		background-color: rgba(97, 108, 110, 0.7);
+		filter: blur(100px);
+		height: 15em;
+	}
 	gliders {
 		position: absolute;
 		display: flex;
@@ -28,11 +34,11 @@
 		width: 100%;
 		justify-content: center;
 		height: 15em;
-		
 	}
 
 	gliders a {
 		font-size: 1.5rem;
+		color: #e5dede;
 		font-weight: bold;
 		width: 10em;
 		text-align: center;
