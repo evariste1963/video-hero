c14c3716a17d55639d9b5d431ead2733a93c000a
evariste1963 <evariste@hotmail.co.uk>
2023-03-10 10:43:43 +0000

    ww

---
+++ src/lib/components/Section4.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { AnchorFm } from 'sveltekit-embed';
+</script>
+
+<div>
+	<AnchorFm
+		episodeUrl="purrfect-dev/embed/episodes/1-31---Delivering-Digital-Content-with-GraphCMS-e14g55c/a-a650v9a"
+		height="100"
+		width="100"
+	/>
+</div>
+
+<style>
+	div {
+		padding: 4em;
+		height: 1200px;
+		width: 1200px;
+		margin: 0 auto;
+	}
+</style>
