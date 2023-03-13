64142ca8b6f7162d8fff14f7dced33e4a6369091
evariste1963 <evariste@hotmail.co.uk>
2023-03-13 19:45:07 +0000

    section 4--> sketch out height

--- src/lib/components/Section4.svelte
+++ src/lib/components/Section4.svelte
@@ -1,3 +1,11 @@
-<div style="height:800px; color:white; text-align:center; margin: 0 auto; font-size:4rem">
-	<p>something here about something or other</p>
+<div style="color:white; text-align:center; margin: 0 auto; font-size:4rem">
+	<br><br>
+    <p>something here about something or other</p>
+    <br><br>
+    <p>something here about something or other</p>
+    <br><br>
+    <p>something here about something or other</p>
+    <br><br>
+    <p>something here about something or other</p>
+    <br><br>
 </div>
